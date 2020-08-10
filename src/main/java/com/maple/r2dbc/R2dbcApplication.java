773f20c067465baa34958168e992f3c0c369be2a
Zijie Jiang <zijie.jiang@thoughtworks.com>
2020-08-10 14:44:32 +0800

    integrate flyway by import postgres driver

--- src/main/java/com/maple/r2dbc/R2dbcApplication.java
+++ src/main/java/com/maple/r2dbc/R2dbcApplication.java
@@ -2,9 +2,11 @@ package com.maple.r2dbc;
 
 import io.r2dbc.spi.ConnectionFactory;
 import lombok.RequiredArgsConstructor;
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,7 @@ public class R2dbcApplication {
 
 //    private final JdbcService service;
     private final R2dbcService service;
+    private final Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(R2dbcApplication.class, args);
@@ -27,9 +30,9 @@ public class R2dbcApplication {
     public Mono<String> getUserById(@PathVariable Long id) {
         // this will return empty; flatMap() isn't execute when mono is void
 //        Mono<Void> mono = Mono.empty();
-//        return mono.flatMap(a -> Mono.just("s"));
+//        return mono.flatMap(v -> Mono.just("success"));
 
-        return service.test().then(Mono.just("success")); // this will return "success"
+        return service.test().map(Foo::getName);
     }
 
     @Bean
@@ -37,5 +40,17 @@ public class R2dbcApplication {
         ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
         initializer.setConnectionFactory(connectionFactory);
         return initializer;
+    }
+
+    // because can't config spring.datasource, so we must inject bean Flyway
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        return new Flyway(Flyway.configure()
+                .baselineOnMigrate(true)
+                .dataSource(
+                        env.getRequiredProperty("spring.flyway.url"),
+                        env.getRequiredProperty("spring.flyway.user"),
+                        env.getRequiredProperty("spring.flyway.password"))
+        );
     }
 }
